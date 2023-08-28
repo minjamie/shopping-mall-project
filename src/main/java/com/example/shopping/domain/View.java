6faa543cf8cbd8077b207a996d16aa209d1007c3
hwaj2 <86288994+hwaj2@users.noreply.github.com>
2023-08-28 23:19:57 +0900

    feat : 조회수 엔티티 추가

--- src/main/java/com/example/shopping/domain/View.java
+++ src/main/java/com/example/shopping/domain/View.java
@@ -2,5 +2,30 @@ package com.example.shopping.domain;
 
 // TODO: 화정님
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "views")
 public class View {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer count;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
 }
