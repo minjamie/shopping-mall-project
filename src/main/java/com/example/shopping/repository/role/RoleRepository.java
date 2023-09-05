830015268c7b3feff385bc3d3ca5c4581a2f6f03
Llimy1 <alsgur990104@gmail.com>
2023-09-05 23:43:26 +0900

    ADD-LMH: 삭제 메소드 추가

--- src/main/java/com/example/shopping/repository/role/RoleRepository.java
+++ src/main/java/com/example/shopping/repository/role/RoleRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
+    void deleteByUserId(Integer userId);
+
 }
