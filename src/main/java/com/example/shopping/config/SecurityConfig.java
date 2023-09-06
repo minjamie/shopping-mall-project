1fe8866e9b606f9fa642da844d926ddd975fb9c9
Llimy1 <alsgur990104@gmail.com>
2023-09-07 00:10:57 +0900

    TEST-LMH: 배포 테스트

--- src/main/java/com/example/shopping/config/SecurityConfig.java
+++ src/main/java/com/example/shopping/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                    .antMatchers("/resources/static/**", "/v1/api/user/*").permitAll()
+                    .antMatchers("/resources/static/**", "/v1/api/*").permitAll()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
