a48c08c7fb8a59076c55537e587d4d09577a3d9b
hwaj2 <86288994+hwaj2@users.noreply.github.com>
2023-09-13 19:29:46 +0900

    feat-SHJ : 상품 목록, 수정

--- src/main/java/com/example/shopping/controller/product/ProductController.java
+++ src/main/java/com/example/shopping/controller/product/ProductController.java
@@ -1,14 +1,48 @@
 package com.example.shopping.controller.product;
 
-import io.swagger.annotations.Api;
+import com.example.shopping.dto.common.CommonResponse;
+import com.example.shopping.dto.common.ResultDto;
+import com.example.shopping.dto.product.ProductListResponseDto;
+import com.example.shopping.dto.product.ProductResponseDto;
+import com.example.shopping.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+/*    @GetMapping("")
+    public ResponseEntity<ResultDto<ProductListResponseDto>> getProductList(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }*/
+
+/*    @GetMapping("/category/{cageroryId}")
+    public ResponseEntity<ResultDto<ProductListResponseDto>> getCategoryProductList(
+            @PathVariable Integer categoryId,
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size){
+        PageRequest pageable = PageRequest.of(page-1, size);
+        CommonResponse productListResponse = productService.getCategoryProductList(categoryId, pageable);
+        ResultDto<Void> result = ResultDto.in(productListResponse.getStatus(), productListResponse.getMessage());
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }*/
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ResultDto<ProductResponseDto>> getProduct(@PathVariable Integer productId){
+        CommonResponse productInfoResponse = productService.getProduct(productId);
+        ResultDto<ProductResponseDto> result = ResultDto.in(productInfoResponse.getStatus(), productInfoResponse.getMessage());
+        result.setData((ProductResponseDto) productInfoResponse.getData());
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 
 
 }
--- src/main/java/com/example/shopping/service/product/ProductService.java
+++ src/main/java/com/example/shopping/service/product/ProductService.java
@@ -1,14 +1,66 @@
 package com.example.shopping.service.product;
 
 
+import com.example.shopping.domain.*;
+import com.example.shopping.dto.common.CommonResponse;
+import com.example.shopping.dto.product.ProductResponseDto;
+import com.example.shopping.repository.brand.BrandRepository;
+import com.example.shopping.repository.category.CategoryRepository;
+import com.example.shopping.repository.image.ImageRepository;
+import com.example.shopping.repository.product.ProductRepository;
+import com.example.shopping.service.error.ErrorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ProductService {
+
+    private final ErrorService errorService;
+    private final ProductRepository productRepository;
+    private final ImageRepository imageRepository;
+    private final BrandRepository brandRepository;
+    private final CategoryRepository categoryRepository;
+
+
+/*
+    public CommonResponse getCategoryProductList(Integer categoryId, Pageable pageable){
+        Optional<Category> category = categoryRepository.findById(categoryId);
+        if(category.isEmpty()){
+            return errorService.createErrorResponse("해당 카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND, null);
+        }
+        return errorService.createSuccessResponse("카테고리 상품목록 조회 완료하였습니다.", HttpStatus.OK, null);
+    }
+*/
+
+
+
+    public CommonResponse getProduct(Integer productId){
+        Optional<Product> product = productRepository.findById(productId);
+        if (product.isEmpty()) {
+            return errorService.createErrorResponse("해당 상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND, null);
+        }
+        List<ProductOption> productOptionList = product.get().getProductOptions();
+        // List<Brand> brandList = product.get().getBrands();
+        Optional<Image> image = imageRepository.findImageByProduct(product.get());
+        if (image.isEmpty()){
+            return errorService.createErrorResponse("해당 상품 이미지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND, null);
+        }
+        Optional<Brand> brand = brandRepository.findBrandByProduct(product.get());
+        if (brand.isEmpty()){
+            return errorService.createErrorResponse("해당 상품 브랜드를 찾을 수 없습니다.", HttpStatus.NOT_FOUND, null);
+        }
+
+        ProductResponseDto productResponseDto = ProductResponseDto.of(product.get(), brand.get(), image.get(), productOptionList);
+        return errorService.createSuccessResponse("판매자 상품조회 완료하였습니다.", HttpStatus.OK, productResponseDto);
+    }
 
 
 }
