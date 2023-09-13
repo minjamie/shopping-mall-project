9ff6f82556a93d97e8224a5242f90c74bc041977
hwaj2 <86288994+hwaj2@users.noreply.github.com>
2023-09-13 15:01:51 +0900

    feat-SHJ : 상품, 카테고리, 서브카테고리 dto 추가 및 변경

--- src/main/java/com/example/shopping/dto/product/ProductListResponseDto.java
+++ src/main/java/com/example/shopping/dto/product/ProductListResponseDto.java
@@ -1,10 +1,16 @@
 package com.example.shopping.dto.product;
 
 
+import com.example.shopping.dto.common.Pagination;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class ProductListResponseDto {
+    private Pagination pagination;
+    private List<ProductResponseDto> productList;
+
 }
---
+++ src/main/java/com/example/shopping/dto/product/ProductRequestDto.java
@@ -0,0 +1,49 @@
+package com.example.shopping.dto.product;
+
+import com.example.shopping.domain.Enum.SellStatus;
+import com.example.shopping.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequestDto {
+    private Integer categoryId;
+    private Integer subcategoryId;
+    private Integer optionId;
+    private String name;
+    private Integer price;
+    private Integer stock;
+    private String description;
+    private SellStatus sellStatus;
+    private Integer discountRate;
+    private Boolean isDiscount;
+    private Boolean isNew;
+    private Integer deliveryPrice;
+    private LocalDateTime saleStartDate;
+    private LocalDateTime saleEndDate;
+    private String imageType;
+    private String imageUrl;
+
+
+    public Product toEntity(){
+        return Product.builder()
+                .name(name)
+                .price(price)
+                .description(description)
+                .sellStatus(sellStatus)
+                .discountRate(discountRate)
+                .isDiscount(isDiscount)
+                .isNew(isNew)
+                .deliveryPrice(deliveryPrice)
+                .saleStartDate(saleStartDate)
+                .saleEndDate(saleEndDate)
+                .build();
+    }
+}
