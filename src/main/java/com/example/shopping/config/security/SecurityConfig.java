23fc6f3be0adda3a1e599b5ff7c0a5338ef74c10
Llimy1 <alsgur990104@gmail.com>
2023-09-14 00:04:49 +0900

    ADD-LMH : 권한 수정

--- src/main/java/com/example/shopping/config/security/SecurityConfig.java
+++ src/main/java/com/example/shopping/config/security/SecurityConfig.java
@@ -45,12 +45,13 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                    .antMatchers("/resources/static/**", "/api/v1/product/*",
-                            "/api/v1/products/*", "/api/v1/user/sign/*", "/api/v1/user/login/*", "/api/v1/user/unlock").permitAll()
-                    .antMatchers("/api/v1/user/*/withdrawal", "/api/v1/user/validate/*"
-                            , "/api/v1/user/*/order", "/api/v1/user/*/cart", "/api/v1/user/*/pay/*",
-                            "/api/v1/user/logout/*", "/api/v1/user/reissue/*", "/api/v1/user/cart/*").hasRole("USER")
-                    .antMatchers("/api/v1/seller/*", "/api/v1/user/seller/*").hasRole("SELLER")
+//                    .antMatchers("/resources/static/**", "/api/v1/product/*",
+//                            "/api/v1/products/*", "/api/v1/user/sign/*", "/api/v1/user/login/*", "/api/v1/user/unlock").permitAll()
+//                    .antMatchers("/api/v1/user/*/withdrawal", "/api/v1/user/validate/*"
+//                            , "/api/v1/user/*/order", "/api/v1/user/*/cart", "/api/v1/user/*/pay/*",
+//                            "/api/v1/user/logout/*", "/api/v1/user/reissue/*", "/api/v1/user/cart/*").hasRole("USER")
+//                    .antMatchers("/api/v1/seller/*", "/api/v1/user/seller/*").hasRole("SELLER")
+                .antMatchers("/ap1/v1/*").permitAll()
                 .and()
                 .exceptionHandling()
                     .accessDeniedHandler(customAccessDeniedHandler)
